25e1c382a36f1d941f3ee2d9fc5a5f3f64c15c9b
Ketan9548 <kc918062@gmail.com>
2025-08-17 20:53:27 +0530

    done changes

--- Array/Find_the_Duplicate_Number.cpp
+++ Array/Find_the_Duplicate_Number.cpp
@@ -1,7 +1,81 @@
-#include<iostream>
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// int main()
+// {
+//     vector<int> v = {1, 3, 4, 2, 2};
+//     int n = v.size();
+//     int left = 0;
+//     int right = n - 1;
+//     while (left <= right)
+//     {
+//         int mid = left + (right - left) / 2;
+//         int count = 0;
+//         for (int i = 0; i < n; i++)
+//         {
+//             if (v[i] <= mid)
+//             {
+//                 count++;
+//             }
+//         }
+//         if (count > mid)
+//         {
+//             right = mid - 1;
+//         }
+//         else
+//         {
+//             left = mid + 1;
+//         }
+//     }
+//     cout << left;
+//     return 0;
+// }
+
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// using namespace std;
+// int main()
+// {
+//     vector<int> v = {1, 3, 4, 2, 2};
+//     int n = v.size();
+//     sort(v.begin(), v.end());
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (v[i] == v[i + 1])
+//         {
+//             cout << v[i];
+//             break;
+//         }
+//     }
+//     return 0;
+// }
+
+#include <iostream>
+#include <vector>
 using namespace std;
 int main()
 {
-              
+    vector<int> v = {1, 3, 4, 2, 2};
+    int n = v.size();
+    int fast = v[0];
+    int slow = v[0];
+    do
+    {
+        slow = v[slow];
+        cout << "slow: " << slow << " ";
+        fast = v[v[fast]];
+        cout << "fast: " << fast << " ";
+    } while (slow != fast);
+
+    fast = v[0];
+    while (slow != fast)
+    {
+        slow = v[slow];
+        fast = v[fast];
+    }
+    cout << endl;
+    cout << fast;
+
     return 0;
 }
